64a6f3ceb4a644e7df84ac40bf73e04a2462308b
NME.eth <maximus.holm@gmail.com>
2022-04-08 22:36:44 +0300

    basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from 'react';
 import {
   ChakraProvider,
   Box,
@@ -8,26 +8,24 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
+    <Box textAlign='center' fontSize='xl'>
+      <Grid minH='100vh' p={3}>
+        <ColorModeSwitcher justifySelf='flex-end' />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Edit <Code fontSize='xl'>src/App.tsx</Code> and save to reload.
           </Text>
           <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+            color='teal.500'
+            href='https://chakra-ui.com'
+            fontSize='2xl'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn Chakra
           </Link>
