238095af15af0565f7259a3422c7de1dacbd0ea5
NME.eth <maximus.holm@gmail.com>
2022-04-10 00:32:54 +0300

    all pages

---
+++ src/pages/web2.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useState } from 'react';
+import {
+  Icon,
+  VStack,
+  HStack,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  useColorModeValue,
+  SimpleGrid,
+} from '@chakra-ui/react';
+
+function Web2() {
+  return (
+    <VStack>
+      Hello Web2
+    </VStack>
+  );
+}
+
+export default Web2;
---
+++ src/pages/web3.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useState } from 'react';
+import {
+  Icon,
+  VStack,
+  HStack,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  useColorModeValue,
+  SimpleGrid,
+} from '@chakra-ui/react';
+
+function Web3() {
+  return (
+    <VStack>
+      Hello Web3
+    </VStack>
+  );
+}
+
+export default Web3;
