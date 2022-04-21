31df90398cbe5a7b515c43c1ae7bee9258b10f81
NME.eth <maximus.holm@gmail.com>
2022-04-21 12:42:46 +0300

    adding project metadata relating to tab name and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Helmet } from 'react-helmet';
 import { theme } from './style/theme';
 import Layout from './layouts/layout';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>0xMouseLess</title>
+        <meta name="description" content="Developer portfolio using react and chakra-ui" />
+      </Helmet>
       <Layout />
     </ChakraProvider>
   );
