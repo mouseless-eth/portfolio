7a9ccf9aa9a779b3595a1a42f459ba52bd87867a
NME.eth <maximus.holm@gmail.com>
2022-05-19 03:10:59 +0300

    removing sniper bot v2 from portfolio

--- src/data/web3Portfolio.ts
+++ src/data/web3Portfolio.ts
@@ -1,6 +1,6 @@
 import NftSummary from '../assets/work/web3/nftsummary.png';
 import IcoSniperV1 from '../assets/work/web3/icosniperv1.png';
-import IcoSniperV2 from '../assets/work/web3/icosniperv2.png';
+// import IcoSniperV2 from '../assets/work/web3/icosniperv2.png';
 import MassMinting from '../assets/work/web3/massminting.png';
 import FlashBotsRecovery from '../assets/work/web3/flashbotsrecovery.png';
 import FlashClaim from '../assets/work/web3/flashclaim.png';
@@ -49,7 +49,7 @@ export const web3: Web3Project[] = [
     image: FlashClaim,
   },
   {
-    title: 'ICO Sniping Bot v1',
+    title: 'ICO Sniping Bot',
     description: 'Monitoring and buying a token as soon as liquidity is added on a DEX',
     badges: [ethersjs, ganache, javascript],
     statuses: [opensource],
@@ -63,13 +63,13 @@ export const web3: Web3Project[] = [
     statuses: [privaterepo, inprogress],
     image: MassMinting,
   },
-  {
-    title: 'ICO Sniping Bot v2',
-    description: 'Implementing 1Inch\'s Router to v1 bot so now it works on all chains+exchanges',
-    badges: [ethersjs, ganache, javascript, flashbots],
-    statuses: [privaterepo],
-    image: IcoSniperV2,
-  },
+  // {
+  //   title: 'ICO Sniping Bot v2',
+  //   description:'Implementing 1Inch\'s Router to v1 bot so now it works on all chains+exchanges',
+  //   badges: [ethersjs, ganache, javascript, flashbots],
+  //   statuses: [privaterepo],
+  //   image: IcoSniperV2,
+  // },
   {
     title: 'NFT summary creator',
     description: 'WebApp that produces clean reports of a user\'s NFT based on it\'s metadata',
