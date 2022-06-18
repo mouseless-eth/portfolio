ef46a4a1ad37b30ce95328dbaf7d3b0b4e58ce3a
NME.eth <maximus.holm@gmail.com>
2022-06-19 01:09:04 +0300

    warning msg

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
-// import {
-//   Text,
-// } from '@chakra-ui/react';
+import {
+  Text,
+} from '@chakra-ui/react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 // import Header from '../components/header';
 
 /* eslint-disable react/jsx-no-constructed-context-values */
 function About() {
   return (
-    <TwitterTimelineEmbed
-      sourceType="profile"
-      screenName="0xMouseLessDev"
-      options={{ width: '400' }}
-    />
+    <>
+      <Text>
+        [In the works... more info coming soon!]
+      </Text>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="0xMouseLessDev"
+        options={{ width: '400' }}
+      />
+    </>
   );
 }
 
